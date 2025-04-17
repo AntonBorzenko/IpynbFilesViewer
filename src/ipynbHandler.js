dcf5440a3798412729becb38eb81f184b0fc514f
Anton Borzenko <borzenkoanton@yandex-team.ru>
2025-04-17 13:57:09 +0200

    Fix plugin from manifest V2 to V3

--- src/ipynbHandler.js
+++ src/ipynbHandler.js
@@ -6,28 +6,31 @@ var avaliableContentTypes = [
 
 function isIpynbFile(details) {
     var pos = details.url.indexOf('.ipynb');
-    
+
     if (pos === -1) {
         return false;
     }
-    
+
     if ( !(pos + 6 === details.url.length || details.url[pos + 6] == '?') ) {
         return false;
     }
-    
+
     var contentType = details.responseHeaders ? details.responseHeaders.filter(obj => obj.name.toLowerCase() === 'content-type') : '';
     contentType = contentType ? contentType[0].value : 'text/html';
     contentType = contentType.toLowerCase().split(';')[0].trim();
     if ( avaliableContentTypes.indexOf(contentType) === -1 ) {
         return false;
     }
-    
+
     return true;
 }
 
-var baseUrl = chrome.extension.getURL('content/ipynb-viewer.html');
+
+const extensionViewerBaseUrl = chrome.runtime.getURL('content/ipynb-viewer.html');
+
+
 function getViewerURL(url) {
-    return baseUrl + '?file=' + encodeURIComponent(url);
+    return `${extensionViewerBaseUrl}?file=${encodeURIComponent(url)}`;
 }
 
 
@@ -36,47 +39,39 @@ chrome.webRequest.onHeadersReceived.addListener(
         if (!isIpynbFile(details)) {
             return;
         }
-        
-        if (details.url.indexOf('ipynb-watch=false') !== -1) {
+
+        if (details.url.includes('ipynb-watch=false')) {
             return;
         }
-        var viewerUrl = getViewerURL(details.url);
-        return {
-            redirectUrl: viewerUrl
-        };
-    }, 
+
+        if (details.tabId >= 0) {
+            chrome.tabs.update(details.tabId, { url: getViewerURL(details.url) });
+        }
+    },
     {
         urls: ["<all_urls>"],
         types: ["main_frame", "sub_frame"]
-    }, 
-    ["responseHeaders", "blocking"]
+    },
+    ["responseHeaders", "extraHeaders"]
 );
+
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    if (details.url.indexOf('ipynb-watch=false') !== -1) {
-        return false;
+    if (details.url.includes('ipynb-watch=false')) {
+        return;
     }
-    
-    var viewerUrl = getViewerURL(details.url);
 
-    return { redirectUrl: viewerUrl, };
+    if (details.tabId >= 0) {
+        chrome.tabs.update(details.tabId, { url: getViewerURL(details.url) });
+    }
   },
   {
     urls: [
       'file://*/*.ipynb',
       'file://*/*.IPYNB',
-      ...(
-        // Duck-typing: MediaError.prototype.message was added in Chrome 59.
-        MediaError.prototype.hasOwnProperty('message') ? [] :
-        [
-          // Note: Chrome 59 has disabled ftp resource loading by default:
-          // https://www.chromestatus.com/feature/5709390967472128
-          'ftp://*/*.ipynb',
-          'ftp://*/*.IPYNB',
-        ]
-      ),
     ],
     types: ['main_frame', 'sub_frame'],
   },
-  ['blocking']);
+  [],
+);
